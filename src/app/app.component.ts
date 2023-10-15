7ec34ee9487e629b284820050f0c6316d29cb3c4
Stellce <andrey.is323@gmail.com>
2023-10-15 02:25:44 +0200

    simple calc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'teacher-helper';
+  numOfGrades: number = 0;
+  grades: number[] = [];
+  isTableGenerated: boolean = false;
+  avgGrade: number;
+
+  onGenerateTable() {
+    for (let i=0; i<this.numOfGrades; i++) {
+      this.grades.push(0);
+    }
+  }
+
+  onCalculate(form: NgForm){
+    this.grades = Object.values(form.value);
+    let sum = this.grades.reduce((acc, grade) => acc += grade);
+    this.avgGrade = sum/this.grades.length;
+    console.log(this.grades);
+  }
+
+  protected readonly Array = Array;
 }
